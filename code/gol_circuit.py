947e1f27c52f3c022ae006f3ef5f3c2b1c79eb9c
helq <linuxero789@gmail.com>
2022-05-16 17:38:21 -0400

    Implementing new GoL circuit with No leak and positive only synapses' weights

--- code/gol_circuit.py
+++ code/gol_circuit.py
@@ -16,13 +16,24 @@ if __name__ == '__main__':
                      [1, 1, 1]]
     t1 = np.array([2.5, 3.5])  # type: ignore
 
+    # NO NEGATIVE WEIGHTS
     heartbeat = 1 / 5
-
     sngol = SNCircuit.load_json(
         'gol/json/gol-nonnegative-v1.json', {'heartbeat': heartbeat})
 
     msaver = ModelSaverLayers(dt=heartbeat)
     msaver.add_conv2d_layer(k1, t1, (20, 20), (1, 1), (1, 1))
     msaver.add_sncircuit_layer(sngol, [(1, 0), (1, 1)])
-    msaver.add_one2one_conn(from_=(2, 4), to=0, weight=1.0)
+    msaver.add_one2one_conn(from_=(2, sngol.outputs[0]), to=0, weight=1.0)
     msaver.save(dump_folder / "gol-20x20-circuit-nonnegative.doryta.bin")
+
+    # NO NEGATIVE WEIGHTS AND NO LEAK
+    heartbeat = 1 / 5
+    sngol2 = SNCircuit.load_json(
+        'gol/json/gol-no-leak.json', {'heartbeat': heartbeat})
+
+    msaver = ModelSaverLayers(dt=heartbeat)
+    msaver.add_conv2d_layer(k1, t1, (20, 20), (1, 1), (1, 1))
+    msaver.add_sncircuit_layer(sngol2, [(1, 0), (1, 1)])
+    msaver.add_one2one_conn(from_=(2, sngol2.outputs[0]), to=0, weight=1.0)
+    msaver.save(dump_folder / "gol-20x20-no-leak.doryta.bin")
